6684f7ff4c898eb5de4f88a6d5a72828731bd719
ElimikaM <elimikamtaani@gmail.com>
2020-06-20 23:20:09 +0300

    WANAFUNZI SOMENI

---
+++ app/src/main/java/com/kscepast/papers/MainActivity.java
@@ -0,0 +1,168 @@
+package com.kscepast.papers;
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+
+import android.app.DownloadManager;
+
+import android.content.pm.PackageManager;
+
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+public class MainActivity extends AppCompatActivity {
+    private WebView web;
+    private InterstitialAd mInterstitialAd;
+    String webUrl = "https://drive.google.com/drive/folders/1oMqFKMOGbK9GA0tL_cHahXCgc6B1oLaL?usp=sharing";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        AdView mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        //
+        prepareAd();
+
+        ScheduledExecutorService scheduler =
+                Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(new Runnable() {
+
+            public void run() {
+                Log.i("hello", "world");
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        if (mInterstitialAd.isLoaded()) {
+                            mInterstitialAd.show();
+                        } else {
+                            Log.d("TAG"," Interstitial not loaded");
+                        }
+
+                        prepareAd();
+
+
+                    }
+                });
+
+            }
+        }, 60, 10, TimeUnit.SECONDS);
+
+
+
+
+
+    }
+
+
+    public void  prepareAd(){
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        //
+        web = (WebView) findViewById(R.id.wv);
+        web.loadUrl(webUrl);
+
+        WebSettings mywebsettings = web.getSettings();
+        mywebsettings.setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+
+        //improve webview performance
+
+        web.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        web.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        web.getSettings().setAppCacheEnabled(true);
+        web.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        mywebsettings.setDomStorageEnabled(true);
+        mywebsettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        mywebsettings.setUseWideViewPort(true);
+        mywebsettings.setSavePassword(true);
+        mywebsettings.setSaveFormData(true);
+        mywebsettings.setEnableSmoothTransition(true);
+
+
+        //External storage permission for saving file
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+
+                Log.d("permission", "permission denied to WRITE_EXTERNAL_STORAGE - requesting it");
+                String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permissions, 1);
+            }
+
+
+        }
+
+        //handle downloading
+
+        web.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setMimeType(mimeType);
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie", cookies);
+                request.addRequestHeader("User-Agent", userAgent);
+                request.setDescription("Downloading file....");
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                assert dm != null;
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(web.canGoBack()){
+
+            web.goBack();
+        }
+        else {
+
+            super.onBackPressed();
+        }
+    }
+}
+
